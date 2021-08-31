39d33ee7108571bccfa76b1c5e6a00e9b07da524
Zestlifia <zestlifia123@gmail.com>
2021-08-31 12:26:18 +0800

    add jukebox & pop initializer

---
+++ jukebox/src/test/java/com/zestlifia/jukebox/JukeboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zestlifia.jukebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JukeboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
